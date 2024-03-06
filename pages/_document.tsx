94c44e27f2b0ac5159be7e95119d3726dff39818
hakim maaouia <hakimmaaouia@gmail.com>
2024-03-06 01:28:52 +0100

    add google manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 import { IconContext } from '@react-icons/all-files'
 
@@ -18,6 +19,19 @@ export default class MyDocument extends Document {
             />
 
             <link rel='manifest' href='/manifest.json' />
+            <Script
+              async
+              src='https://www.googletagmanager.com/gtag/js?id=G-09FJVF9NMV'
+            />
+
+            <Script id='google-analytics'>
+              {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-09FJVF9NMV');
+          `}
+            </Script>
           </Head>
 
           <body>
